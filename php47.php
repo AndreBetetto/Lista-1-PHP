9178a7689b161c72077ddb10916ffd5670fc9d98
AndreBetetto <andre.betetto@unesp.br>
2022-05-10 14:10:34 -0300

    good

--- php47.php
+++ php47.php
@@ -1,19 +1,16 @@
 <?php
 $nome = "$_POST[nome]"; //nome
 $velocidade = "$_POST[velocidade]";
+$velMax = "$_POST[velocidadeMax]";
+$int_v = (int) $velocidade;
+if($int_v > $velMax + 34)
+    echo"Multa de 200 reais";
+else if($int_v > $velMax + 11 && $int_v < $velMax + 34)
+    echo"Multa de R$100";
+else if($int_v > $velMax +10)
+    echo"Multa de R$50!!!!!";
+    else
+    echo"Sem multa!";
 
-if(velocidade >= 70)
-{
-    echo"Valor a ser pago 2x mais";
-}
-else
-if(velocidade >= 60)
-{
-    echo"Valor a ser pago 1x mais";
-}
-else
-if(velocidade < 60)
-{
-    echo"Valor normal";
-}
+
 ?>
