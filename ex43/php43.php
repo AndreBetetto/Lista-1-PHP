6c0be09ef6d626f844c293d0b9b2e8e4f3de10e5
AndreBetetto <andre.betetto@unesp.br>
2022-05-26 10:59:17 -0300

    f

--- ex43/php43.php
+++ ex43/php43.php
@@ -6,8 +6,9 @@
     <link rel ="stylesheet" type ="text/css" href="estilo.css">
 </head>
 <body>
+    <h3>
     <?php
-include("estilo.css");
+//include("estilo.css");
     $altura = "$_POST[alt]";
     $peso = "$_POST[peso]";
 
@@ -19,27 +20,28 @@ include("estilo.css");
     echo"<br>Seu IMC é: ".$imc;
     echo"<br><br><br><br>";
     if($imc < 18.5)
-        echo"IMC menor que 18.5, Magreza";
+        echo"IMC menor que 18,5, Magreza";
     else
     if(18.5< $imc && $imc <= 24.9)
-        echo"IMC entre 18,5 e 24,9";
+        echo"IMC entre 18,5 e 24,9 --> Normal";
     else
     if(24.9 < $imc && $imc <= 29.9) 
-        echo"IMC entre 25 e 29,9";
+        echo"IMC entre 25 e 29,9 --> Sobrepeso";
     else
     if(18.5< $imc && $imc <= 24.9)
-        echo"IMC entre 18,5 e 24,9";
+        echo"IMC entre 18,5 e 24,9 --> Obesidade";
     else       
     if(24.9 < $imc && $imc <= 29.9)
-        echo"IMC entre 25 e 39.99";
+        echo"IMC entre 25 e 39.99 --> Obesidade II";
     else
     if(30 > $imc && $imc <= 40)  
-        echo"IMC entre 25 e 39.99";
+        echo"IMC entre 25 e 39.99 --> Obesidade III" ;
         else
     if($imc > 40)  
         echo"IMC maior que 40";
     echo"<br><br><br><br>https://www.saudenaosepesa.com.br/diagnostico.html?utm_source=g-search&utm_medium=cpc&utm_content=texto&utm_term=normal&utm_campaign=ogilvybr_novo-nordisk_always-on-performance_g-search_cliques_202205_texto_cpc_calcule-imc";    
 ?>
+</h3>
 </body>
 </html>
 
